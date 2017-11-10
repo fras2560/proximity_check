ac1f36e2139dec3e5ba3af9db178c50eeafbc105
fras2560 <dallas_j_frase@hotmail.com>
2017-11-10 12:24:32 -0500

    Updated the test

--- src/testRunCheck.py
+++ src/testRunCheck.py
@@ -29,10 +29,13 @@ class Test(unittest.TestCase):
                   self.query,
                   self.result,
                   ".html")
+        expect = ["Query,Document,Doc-Length,Ranking,Span,Min-Span,Normalized-Span,Normalized-Min-Span,Min-Distance,Ave-Distance,Max-Distance",
+                  "NTCIR11-Math-1,27473.html,4728,NOT RELEVANT,668,495,55.666666666666664,99.0,1,332.67857142857144,668",
+                  "NTCIR11-Math-1,02459.html,1822,RELEVANT,1017,9,53.526315789473685,0.9,1,148.58333333333334,1017"]
         with open(self.output) as f:
             lines = 0
-            for __ in f:
-                print(__)
+            for index, line in enumerate(f):
+                self.assertEqual(expect[index].strip(), line.strip())
                 lines += 1
             self.assertEqual(lines, 3)
 
